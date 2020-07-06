0b4b899a042d893ffc2653947bc92ffdcf2bedf4
Cristian Gutiérrez <cristian.gutierrez5@virginiogomez.cl>
2020-07-06 02:10:09 -0400

    Agregar archivos de proyecto.

---
+++ MetroUIPrueba2/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MetroUIPrueba2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Información de la empresa.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Pagina para contacto.";
+
+            return View();
+        }
+        public ActionResult Registrar()
+        {
+            ViewBag.Message = "Pagina para registrar usuario.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Pagina para iniciar sesión.";
+
+            return View();
+        }
+        public ActionResult Mesas()
+        {
+            ViewBag.Message = "Pagina con mesas a la venta.";
+
+            return View();
+        }
+        public ActionResult Comedores()
+        {
+            ViewBag.Message = "Pagina con comedores a la venta.";
+
+            return View();
+        }
+        public ActionResult Closets()
+        {
+            ViewBag.Message = "Pagina con closets a la venta.";
+
+            return View();
+        }
+        public ActionResult Prueba()
+        {
+            ViewBag.Message = "Pagina para probar interfaz.";
+
+            return View();
+        }
+        public ActionResult Crear()
+        {
+            ViewBag.Message = "Pagina para creae muebles.";
+
+            return View();
+        }
+    }
+}
